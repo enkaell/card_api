50287755da93e6f7ea2309073dfc297e2f81e0c0
vladislav_chesnokov <vlad2012chesnokov@mail.ru>
2022-11-12 14:32:54 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from typing import Union
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+
+app = FastAPI()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
