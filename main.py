230ec5e55a741c725c3e03d537e7423884a8af65
vladislav_chesnokov <vlad2012chesnokov@mail.ru>
2022-11-13 21:21:34 +0500

    Added alembic and migrations

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 from fastapi import FastAPI
 from fastapi.security import OAuth2PasswordBearer
--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from main import db, Base
-from sqlalchemy import (Column, ForeignKey, Integer, String, Date)
+from sqlalchemy import (Column, ForeignKey, Integer, String, Date, Identity, text)
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 import uuid
@@ -8,7 +8,7 @@ from sqlalchemy.orm import Session
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, Identity(start=1), primary_key=True)
     username = Column(String(25), nullable=False)
     name = Column(String(), nullable=False)
     surname = Column(String(), nullable=False)
@@ -16,31 +16,30 @@ class User(Base):
     dateborn = Column(Date(), nullable=False)
     sex = Column(String(), nullable=False)
     email = Column(String(), nullable=False)
-    token = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    token = Column(
+        UUID(as_uuid=False),
+        server_default=text("gen_random_uuid()"),
+        unique=True,
+        nullable=False,
+        index=True,
+    )
     organization = relationship("Organization", secondary="org_to_users")
 
 
 class Organization(Base):
     __tablename__ = "organizations"
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, Identity(start=1), primary_key=True, autoincrement=True)
     name = Column(String, nullable=False)
     owner = relationship("User", secondary="org_to_users")
 
 
 class OrgUser(Base):
     __tablename__ = "org_to_users"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, Identity(start=1), primary_key=True)
     org_id = Column(Integer, ForeignKey("organizations.id"))
     user_id = Column(Integer, ForeignKey("users.id"))
 
 
 Base.metadata.create_all(db)
 
-with Session(bind=db) as session:
-    u1 = User(id=1, username="vlad2012", name="Vlad", surname="Chesnokov",
-              dateborn="11.11.2011", sex="M", email="vlad@mail.ru")
-    session.add(u1)
-    o1 = Organization(name="Gay Club")
-    session.add(o1)
-    o1.owner = [u1]
-    session.commit()
+# add fixtures
