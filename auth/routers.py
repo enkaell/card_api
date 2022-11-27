7748a3577122ef8c9f85ce60b2db63e9f1dbe5fb
Карманов В.И <vi.karmanov@tensor.ru>
2022-11-28 02:26:00 +0500

    Проброска токена в ответ

--- auth/routers.py
+++ auth/routers.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from fastapi import Depends, APIRouter
-from db.models import User, get_session
+from requests import User
+from db.models import get_session
 from fastapi.responses import JSONResponse
 from fastapi.security import OAuth2PasswordRequestForm
 from auth.utils import validate_create_user, user_login
@@ -11,15 +12,15 @@ router = APIRouter()
 @router.post("/login")
 async def login(form_data: OAuth2PasswordRequestForm = Depends(), session: Session = Depends(get_session)):
     auth = user_login(form_data, session)
-    headers = {"X-Access-token": auth.get("access_token"), "X-Refresh-token": auth.get('refresh_token')}
-    return JSONResponse(content={"message": "Successful login"}, headers=headers)
+    auth.update({"message": "Successful login"})
+    return JSONResponse(content=auth)
 
 
 @router.post("/register")
 async def register(form_data: User, session: Session = Depends(get_session)) -> object:
     auth = validate_create_user(form_data, session)
-    headers = {"X-Access-token": auth.get("access_token"), "X-Refresh-token": auth.get('refresh_token')}
-    return JSONResponse(content={"message": "User is created"}, headers=headers)
+    auth.update({"message": "User is created"})
+    return JSONResponse(content=auth)
 # todo: /logout Token = None
 # todo: /register
 
--- auth/utils.py
+++ auth/utils.py
@@ -12,16 +12,16 @@ from fastapi.security import OAuth2PasswordRequestForm
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 1
+ACCESS_TOKEN_EXPIRE_MINUTES = 1440
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def create_token(data: dict, expires_delta: Union[timedelta, None] = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.utcnow() + timedelta(minutes=expires_delta)
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
@@ -41,7 +41,7 @@ def user_login(form_data: OAuth2PasswordRequestForm, session: Session):
         validate_password(user, form_data.password)
     else:
         raise HTTPException(status_code=404, detail="Incorrect username or password")
-    return {'access_token': user.token, 'refresh_token': user.refresh_token}
+    return {'access_token': user.token, 'refresh_token': user.refresh_token, 'token_type': 'bearer'}
 
 
 def validate_create_user(user: User, session: Session):
@@ -55,12 +55,11 @@ def validate_create_user(user: User, session: Session):
         'name': user.name,
         'surname': user.surname,
         'last_name': user.last_name,
-        'sex': user.sex,
-        'email': user.email,
-        'password': get_password_hash(user.password)
+        'sex': user.sex, 'email': user.email,
+        'password': get_password_hash(user.password),
+        'token': create_token(data={'sub': user.username}),
+        'refresh_token': create_token(data={'sub': user.email}, expires_delta=ACCESS_TOKEN_EXPIRE_MINUTES*7)
     }
-    data['token'] = create_token(data)
-    data['refresh_token'] = create_token(data)
 
     session.add(UserModel(**data))
-    return {'access_token': data['token'], 'refresh_token': data['refresh_token']}
+    return {'access_token': data['token'], 'refresh_token': data['refresh_token'], 'token_type': 'bearer'}
