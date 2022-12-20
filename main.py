2135b237e9ed0057054750b5c60b035fb5bbfd99
Карманов В.И <vi.karmanov@tensor.ru>
2022-12-21 03:47:48 +0500

    Разрешил CORS

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import uvicorn
 import routers
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 oauth2_scheme = routers.oauth2_scheme
 app.include_router(routers.router)
 
