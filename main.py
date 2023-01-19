58014360c229d24b65f22e97395d8f851161f24a
Vladislav <vladkarmanov772@gmail.com>
2023-01-19 20:19:05 +0500

    Вроде как норм слил ветки

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 
+origins = ["*"]
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["POST", "PUT", "GET", "DELETE"],
     allow_headers=["*"]
 )
 oauth2_scheme = routers.oauth2_scheme
 app.include_router(routers.router)
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
